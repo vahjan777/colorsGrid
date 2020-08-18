13d488741d031b17b3300be7a34eb4a26b9090e6
vahjan777 <vahjan777@gmail.com>
2020-08-18 11:53:55 +0400

    colorsGrid

--- index.js
+++ index.js
@@ -1,16 +1,25 @@
-const rgbArray = 255;
+const rgbcolor = 255;
 
-function createMatrix() {
+function  createMatrix() {
     const mainDiv = document.getElementById('grid');
-    let grid = '';
-    for (let i = 0; i <= rgbArray; i++) {
-        let row = '<div class="row">';
-        for (let j = 0; j <= rgbArray; j++) {
-            row = row + `<div class="box" title="RGB (0, ${i}, ${j})" style="background-color:rgb(0, ${i}, ${j})"></div>`;
+
+    for (let i = 0; i <= rgbcolor; i++) {
+        const row = document.createElement('div');
+        row.classList.add('row');
+
+        for (let j = 0; j <= rgbcolor; j++) {
+            const cell = document.createElement('div');
+            cell.classList.add('box');
+            cell.setAttribute('id', `${i}_${j}`);
+            cell.style.backgroundColor =  `rgb(0, ${i}, ${j} )`;
+
+            cell.addEventListener("mouseover",()=>{
+                document.getElementById(`${i}_${j}`).innerHTML = (`RGB (0, ${i}, ${j})`);
+            });
+            row.appendChild(cell);
         }
-        grid = grid + row + '</div>';
+        mainDiv.appendChild(row);
     }
-    mainDiv.innerHTML = grid;
 }
 
 createMatrix();
